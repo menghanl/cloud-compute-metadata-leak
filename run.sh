b7e2fee836f51b4a3b495252a262be47aa52acae
Cody Oss <codyoss@google.com>
2022-06-15 09:43:22 -0500

    testing

---
+++ run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+go test . -run TestML -race -cpu 1,4 -count 1 -v
+go test . -run TestML -race -cpu 1,4 -count 1 -v
+go test . -run TestML -race -cpu 1,4 -count 1 -v
+go test . -run TestML -race -cpu 1,4 -count 1 -v
+go test . -run TestML -race -cpu 1,4 -count 1 -v
+go test . -run TestML -race -cpu 1,4 -count 1 -v
+go test . -run TestML -race -cpu 1,4 -count 1 -v
+go test . -run TestML -race -cpu 1,4 -count 1 -v
+go test . -run TestML -race -cpu 1,4 -count 1 -v
+go test . -run TestML -race -cpu 1,4 -count 1 -v
